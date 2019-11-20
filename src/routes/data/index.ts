5872b9b03b361a4c677a5a3a015cc546be9bc228
HADMARINE <joonlee031@live.com>
2019-11-20 17:05:49 +0900

    /data : minor change

--- src/routes/data/index.ts
+++ src/routes/data/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
+import User, { UserDocument } from '../../lib/models/User';
 import Data, { DataDocument } from '../../lib/models/Data';
 import bodyParser from 'body-parser';
 import throwError from '../../lib/throwError';
@@ -9,16 +10,17 @@ router.use(bodyParser.json());
 
 router.get('/:type', async (req, res, next) => {
   const type = req.params.type;
-  let query = { type };
+  let query = { type: type };
 
   try {
     let data: any;
     try {
       // tslint:disable-next-line: await-promise
-      data = await Data.find(type).sort('title');
+      data = await Data.find(query).sort('title');
     } catch (e) {
       return throwError('데이터를 불러오는 데 실패했습니다.', 500);
     }
+    res.json({ result: data });
   } catch (e) {
     next(e);
   }
